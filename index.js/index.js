7fa221a41a327123097ad511d3dae68892f6fe6f
Danilo-Lopes-Stabile <danlopes95@hotmail.com>
2024-05-29 11:39:17 -0300

    Update index.js
    colocando try e catch e throw new Error

--- index.js/index.js
+++ index.js/index.js
@@ -1,31 +1,45 @@
-
-async function GeradorDeMensagem() {
+async function geradorDeMensagem() {
     const url = "https://api.adviceslip.com/advice"
     const resposta = await fetch(url)
     const json = await resposta.json()
-     console.log(json)
-     return json
+
+    return json;
 }
 
 
-async function Mensagem (){
-    const imagens = await GeradorDeMensagem()
-    
+async function Mensagem() {
+    const imagens = await geradorDeMensagem()
     const texto = imagens.slip.advice
     document.getElementById("Texto").innerText = texto
-    console.log(texto)
+    
+     
 }
 
 
-Mensagem()
+const botao = document.getElementById("botao")
+botao.addEventListener("click", () => {
+    gerarAMensagem()
+})
 
 
-
-function trocaDeMensagem (){
-    const botao = document.getElementById("botao")
-  botao.addEventListener("click",()=>{
-        Mensagem()
-    })
+async function gerarAMensagem() {
+   
+   
+    try {
+        await geradorDeMensagem();
+        await Mensagem();
+        const url = "https://api.adviceslip.com/advice"
+        const resposta = await fetch(url)
+// eu chamei a api novamente para fazer o teste de resposta.ok pois como eu fiz meu codigo separado foi a unica maneira -
+if (!resposta.ok){
+    throw new Error("teste errou")
 }
 
-trocaDeMensagem()
+    } catch (error) {
+        console.log(error)
+    } finally {
+        console.log("a troca foi realizada com sucesso")
+    }
+}
+
+gerarAMensagem()   
